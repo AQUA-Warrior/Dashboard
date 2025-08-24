7ad3b715517376dbe1b352ea800132edeea81134
AQUA-Warrior <ahmed.farooq@protonmail.com>
2025-08-24 12:00:18 -0400

    removed helmet - local development

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const apiRouter = require('./api');
-const helmet = require('helmet');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.set('trust proxy', 1);
-
-app.use(
-  helmet({
-    crossOriginOpenerPolicy: { policy: 'same-origin' },
-    originAgentCluster: true,
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'"],
-      },
-    },
-  })
-);
 
 app.use(express.static('public'));
 
